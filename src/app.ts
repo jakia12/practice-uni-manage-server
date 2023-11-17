01e07acfc35795682f756b05a51d5a69e6b9848e
jakia12 <owasi110@gmail.com>
2023-11-17 07:48:22 +0600

    error solved

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
 //middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("server is running on 5000");
-});
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('server is running on 5000')
+  next()
+})
 
-export default app;
+export default app
